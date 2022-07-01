031c9f33f26ee070bc2b06cf7bfec02ef48b98e6
alex-bartleynees <alexbartleynees@gmail.com>
2022-07-02 09:48:32 +1200

    Add sanity schema for projects

---
+++ studio/schemas/projects.js
@@ -0,0 +1,70 @@
+export default {
+  name: "projects",
+  title: "Projects",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Project Title",
+      type: "string",
+    },
+    {
+      name: "images",
+      type: "array",
+      title: "Images",
+      of: [
+        {
+          name: "image",
+          type: "image",
+          title: "Image",
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: "title",
+              type: "string",
+              title: "Title",
+            },
+            {
+              name: "description",
+              type: "text",
+              title: "Description",
+            },
+            {
+              name: "credits",
+              type: "string",
+              title: "Credits",
+            },
+            {
+              name: "alt",
+              type: "string",
+              title: "Alternative text",
+            },
+          ],
+        },
+      ],
+      options: {
+        layout: "grid",
+      },
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Description",
+    },
+  ],
+  preview: {
+    select: {
+      images: "images",
+      image: "images.0",
+    },
+    prepare(selection) {
+      const { image } = selection;
+
+      return {
+        media: image,
+      };
+    },
+  },
+};
